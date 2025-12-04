8c5e65f5dc4beae2ab5738c5a58ff783ffcbbb21
Daniel Formoso <118393625+Daniel-Formoso@users.noreply.github.com>
2025-12-04 07:08:52 -0300

    adicao de mais cards na tela inicial

--- components/ProcessCard/ProcessCard.tsx
+++ components/ProcessCard/ProcessCard.tsx
@@ -6,6 +6,7 @@ import styles from "./ProcessCard.module.css";
 interface ProcessCardProps {
   icon: React.ReactNode;
   title: string;
+  processType: string;
   description: string;
   onClick?: () => void;
 }
@@ -13,12 +14,14 @@ interface ProcessCardProps {
 export default function ProcessCard({
   icon,
   title,
+  processType,
   description,
   onClick,
 }: ProcessCardProps) {
   return (
     <button className={styles.card} onClick={onClick}>
       <div className={styles.iconWrapper}>{icon}</div>
+      <p className={styles.processType}>{processType}</p>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
     </button>
